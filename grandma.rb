9c9ee9ecb48300643e3348bbef2064b11312a756
Flora Petersen <floramarina@comcast.net>
2020-05-28 04:09:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if "phrase" == "phrase".downcase
+  if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif "phrase" == "phrase".upcase
     return "NO, NOT SINCE 1938!"
