bd83e23b7b84f8e41b575063b8aab4113524577e
Flora Petersen <floramarina@comcast.net>
2020-05-28 04:05:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,13 @@
 def speak_to_grandma(phrase)
   if "phrase" == "phrase".downcase
     return "HUH?! SPEAK UP, SONNY!"
+  end
   elsif "phrase" == "phrase".upcase
     return "NO, NOT SINCE 1938!"
+  end
   elsif "phrase" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 
 
